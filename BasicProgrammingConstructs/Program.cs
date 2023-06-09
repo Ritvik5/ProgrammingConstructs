1c5312065c2314a809a90ecce6bcd6c305dfffe3
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-09 23:21:11 +0530

    [Ritvik]Add: Welcome Message

---
+++ BasicProgrammingConstructs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicProgrammingConstructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Programming Constructs");
+        }
+    }
+}
